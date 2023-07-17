c96901ee0997db250ed44653d882e028199c817f
y_dove <1352035939@qq.com>
2023-07-17 22:35:05 +0800

    写完了/TODO:标准化

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 def read_date():
     df = pd.read_excel('data.xlsx')
@@ -17,3 +18,24 @@ def read_date():
         node.append(_tmp)
     return np.array(node)
 data = read_date()
+
+n_cluster = 6
+
+kmeans = KMeans(n_clusters=n_cluster, random_state=0)
+cluster = kmeans.fit(data)
+
+y_pred = cluster.labels_
+color = ['blue','red','yellow','pink','black','white','gray']
+
+# print(y_pred)
+image = plt.subplots(1)[1]
+
+for i in range(n_cluster):
+    image.scatter(data[y_pred==i,0],data[y_pred==i,1],
+                  marker = 'o',
+                  c = color[i])
+
+# image.show()
+image.set_xlabel('rank')
+image.set_ylabel('gpa')
+plt.show()
